1ecac477c2498fe97f2f352e3535dadfd058520e
Sergey Linnik <sergey.linnik@onlyoffice.com>
2018-10-05 16:08:46 +0300

    fix comment

--- controller/settingscontroller.php
+++ controller/settingscontroller.php
@@ -134,6 +134,7 @@ class SettingsController extends Controller {
      * @param string $storageUrl - ownCloud address available from document server
      * @param string $secret - secret key for signature
      * @param string $defFormats - formats array with default action
+     * @param boolean $sameTab - open in same tab
      *
      * @return array
      */

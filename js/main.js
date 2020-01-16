311c55268ad2026fc7ca26921dad5d56a57e974e
Sergey Linnik <sergey.linnik@onlyoffice.com>
2020-01-16 19:10:31 +0300

    sidebar and closing inline editor for DS < 5.5 (#204)

--- js/main.js
+++ js/main.js
@@ -115,6 +115,23 @@
             $("html, body").scrollTop(0);
 
             OCA.Onlyoffice.folderUrl = location.href;
+
+            var wrapper = $("<div id='onlyofficeHeader' />")
+
+            var btnClose = $("<a class='icon icon-close'></a>");
+            btnClose.on("click", function() {
+                OCA.Onlyoffice.CloseEditor();
+            });
+            wrapper.prepend(btnClose);
+
+            var btnShare = $("<a class='icon icon-shared'></a>");
+            btnShare.on("click", function () {
+                OCA.Onlyoffice.OpenShareDialog();
+            })
+            wrapper.prepend(btnShare);
+
+            $("#header").append(wrapper);
+
             window.history.pushState(null, null, url);
         }
     };
@@ -123,6 +140,7 @@
         $("body").removeClass("onlyoffice-inline");
 
         $("#onlyofficeFrame").remove();
+        $("#onlyofficeHeader").remove();
 
         OCA.Onlyoffice.context = null;
 

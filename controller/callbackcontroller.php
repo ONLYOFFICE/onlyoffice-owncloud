a366dc04fa502bde8a357140fe3d6f8fa02323f3
Sergey Linnik <sergey.linnik@onlyoffice.com>
2020-10-02 18:44:30 +0300

    hooks for shared link

--- controller/callbackcontroller.php
+++ controller/callbackcontroller.php
@@ -214,8 +214,11 @@ class CallbackController extends Controller {
         }
 
         $userId = null;
+
+        $user = null;
         if ($this->userSession->isLoggedIn()) {
-            $userId = $this->userSession->getUser()->getUID();
+            $user = $this->userSession->getUser();
+            $userId = $user->getUID();
         } else {
             \OC_Util::tearDownFS();
 
@@ -244,6 +247,14 @@ class CallbackController extends Controller {
         if ($this->userSession->isLoggedIn() && !$file->isReadable()) {
             $this->logger->error("Download without access right", ["app" => $this->appName]);
             return new JSONResponse(["message" => $this->trans->t("Access denied")], Http::STATUS_FORBIDDEN);
+        }
+
+        if (empty($user)
+            && $this->config->checkEncryptionModule() !== "master") {
+            $owner = $file->getFileInfo()->getOwner();
+            if ($owner !== null) {
+                \OC_Util::setupFS($owner->getUID());
+            }
         }
 
         if ($changes) {

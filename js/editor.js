afa42d14c58063e276aee6597b38365d4bffd71f
Sergey Linnik <sergey.linnik@onlyoffice.com>
2021-04-16 17:37:49 +0300

    Revert "hide goback for old editor"
    This reverts commit 2f6049017855193b6daf50b992e2fc7356d0c2c8.
    
    # Conflicts:
    #	controller/editorcontroller.php

--- js/editor.js
+++ js/editor.js
@@ -65,13 +65,7 @@
         }
 
         if (OCA.Onlyoffice.inframe) {
-            var dsVersion = DocsAPI.DocEditor.version();
-            var versionArray = dsVersion.split(".");
-            if (versionArray[0] < 5 || versionArray[0] == 5 && versionArray[1] < 5) {
-                params.push("inframe=2");
-            } else {
-                params.push("inframe=1");
-            }
+            params.push("inframe=true");
         }
 
         if (OCA.Onlyoffice.Desktop) {

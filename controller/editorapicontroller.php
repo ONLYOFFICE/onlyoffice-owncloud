e2d9fb3fafea8577c44a11e0466d0828ac9de9b1
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2021-09-16 13:29:58 +0300

    check user allow groups in config

--- controller/editorapicontroller.php
+++ controller/editorapicontroller.php
@@ -242,10 +242,6 @@ class EditorApiController extends OCSController {
      */
     public function config($fileId, $filePath = null, $shareToken = null, $version = 0, $inframe = false, $desktop = false, $template = false, $anchor = null) {
 
-        if (empty($shareToken) && !$this->config->isUserAllowedToUse()) {
-            return new JSONResponse(["error" => $this->trans->t("Not permitted")]);
-        }
-
         $user = $this->userSession->getUser();
         $userId = null;
         if (!empty($user)) {
@@ -257,6 +253,14 @@ class EditorApiController extends OCSController {
         if (isset($error)) {
             $this->logger->error("Config: $fileId $error", ["app" => $this->appName]);
             return new JSONResponse(["error" => $error]);
+        }
+
+        $checkUserAllowGroups = $userId;
+        if (!empty($share)) {
+            $checkUserAllowGroups = $share->getSharedBy();
+        }
+        if (!$this->config->isUserAllowedToUse($checkUserAllowGroups)) {
+            return new JSONResponse(["error" => $this->trans->t("Not permitted")]);
         }
 
         $fileName = $file->getName();

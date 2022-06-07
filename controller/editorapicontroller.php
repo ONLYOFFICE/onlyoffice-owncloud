18b4b52efc4360fdb7a45e8674aac42fd79a1061
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2022-06-07 11:05:33 +0300

    added security section with macros to settings page

--- controller/editorapicontroller.php
+++ controller/editorapicontroller.php
@@ -691,6 +691,11 @@ class EditorApiController extends OCSController {
             $params["editorConfig"]["customization"]["toolbarNoTabs"] = true;
         }
 
+        //default is true
+        if($this->config->GetCustomizationMacros() === false) {
+            $params["editorConfig"]["customization"]["macros"] = false;
+        }
+
 
         /* from system config */
 

66df7f459fc4c70548168360489b7e67e2d05c37
Sergey Linnik <sergey.linnik@onlyoffice.com>
2024-02-14 17:36:27 +0300

    Form template rename as PDF form

--- js/main.js
+++ js/main.js
@@ -284,7 +284,7 @@
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
         ];
 
-        OC.dialogs.filepicker(t(OCA.Onlyoffice.AppName, "Create new Form template"),
+        OC.dialogs.filepicker(t(OCA.Onlyoffice.AppName, "Create new PDF form"),
             function (filePath) {
                 OCA.Onlyoffice.CreateFile(name, fileList, 0, filePath);
             },
@@ -459,8 +459,8 @@
 
             menu.addMenuEntry({
                 id: "onlyofficeDocxf",
-                displayName: t(OCA.Onlyoffice.AppName, "Form template"),
-                templateName: t(OCA.Onlyoffice.AppName, "Form template"),
+                displayName: t(OCA.Onlyoffice.AppName, "PDF form"),
+                templateName: t(OCA.Onlyoffice.AppName, "PDF form"),
                 iconClass: "icon-onlyoffice-new-docxf",
                 fileType: "docxf",
                 actionHandler: function (name) {
@@ -471,8 +471,8 @@
             if (!$("#isPublic").val()) {
                 menu.addMenuEntry({
                     id: "onlyofficeDocxfExist",
-                    displayName: t(OCA.Onlyoffice.AppName, "Form template from existing text file"),
-                    templateName: t(OCA.Onlyoffice.AppName, "Form template from existing text file"),
+                    displayName: t(OCA.Onlyoffice.AppName, "PDF form from existing text file"),
+                    templateName: t(OCA.Onlyoffice.AppName, "PDF form from existing text file"),
                     iconClass: "icon-onlyoffice-new-docxf",
                     fileType: "docxf",
                     actionHandler: function (name) {

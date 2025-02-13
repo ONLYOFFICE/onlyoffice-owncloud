e7c27afc25293ed092388e1def238e76a438f1ff
rivexe <stepan.mayorov@onlyoffice.com>
2025-02-13 09:44:57 +0300

    feat: insert-text in onRequestSelectDocument support

--- js/listener.js
+++ js/listener.js
@@ -76,8 +76,14 @@
       case "combine":
         title = t(OCA.Onlyoffice.AppName, "Select file to combine");
         break;
-      default:
+      case "compare":
         title = t(OCA.Onlyoffice.AppName, "Select file to compare");
+        break;
+      case "insert-text":
+        title = t(OCA.Onlyoffice.AppName, "Select file to insert text");
+        break;
+      default:
+        title = t(OCA.Onlyoffice.AppName, "Select file");
     }
     OC.dialogs.filepicker(
       title,
